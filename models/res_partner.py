4a36394722d0c11365c57dee651f10ba6d5488a4
Dimasdidanfebianto <dimaskanjeng113@gmail.com>
2023-11-27 21:28:52 +0700

    refactor

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,8 +4,16 @@ from odoo import models, fields ,api
 class PartnerInherit(models.Model):
     _inherit = 'res.partner'
     
-    id_card = fields.Binary(string='Foto KTP')
-    
+    id_card = fields.Char(string='No Seri Kartu Tim Sukses')
     id_kk = fields.Char(string='No Kartu Keluarga')
     no_tps = fields.Char(string='No TPS')
+    no_ktp = fields.Char(string='No KTP')
+    id_kecamatan = fields.Char(string='Kecamatan')
+    id_desa = fields.Char(string='Desa')
+    id_foto = fields.Binary(string='Foto Kartu Tim Sukses')
+    state= fields.Selection([
+        ('active', 'Active'),
+        ('unactive', 'Unactive'),
+    ], string='Status Bantuan', default='unactive')
+    
     
